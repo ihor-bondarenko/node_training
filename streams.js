ade1b38fe36bcc93cbdd27a98a9e207dd26e299f
Ihor Bondarenko <ihor.bondarenko@acropolium.com>
2017-11-21 19:24:17 +0200

    streams

--- streams.js
+++ streams.js
@@ -2,7 +2,30 @@ const fs = require('fs');
 const zlib = require('zlib');
 const http = require('http');
 const path = require('path');
-const file = 'cld';
+const stream = require('stream');
+const Chance = require('chance');
+
+const chance = new Chance();
+class RandomStream extends stream.Readable {
+  constructor(options) {
+    super(options);
+  }
+  _read(size) {
+    const chunk = chance.string();
+     //[1]
+    console.log(`Pushing chunk of size: ${chunk.length}`);
+    this.push(chunk, 'utf8');
+     //[2]
+    if(chance.bool({likelihood: 5})) {
+       //[3]
+      this.push(null);
+    }
+  }
+}
+module.exports = RandomStream;
+
+
+/*const file = 'cld';
 const server = '127.0.0.1';
 const options = {
   hostname: server,
@@ -24,3 +47,4 @@ fs.createReadStream(file)
 .on('finish', () => {
 console.log('File successfully sent');
 });
+*/
