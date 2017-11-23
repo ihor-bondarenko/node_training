607273f61afa73b74433d7d7633621403d698a00
acro.ihor.bondarenko@gmail.com <acro.ihor.bondarenko@gmail.com>
2017-11-23 15:43:45 +0200

    typescript

---
+++ ts/index.ts
@@ -0,0 +1,13 @@
+interface Type1 {
+    name: string,
+    id: number
+}
+
+function func1(types_arr: Type1[], opt?: any): void {
+    console.log(opt)
+    console.log(types_arr)
+}
+
+const types: Type1[] = [
+    
+];
